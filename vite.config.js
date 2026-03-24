08b8d19df73174836396f16f5e75153260807e62
JuanJo Contreras <82116703+JuanJoContreras@users.noreply.github.com>
2026-03-23 22:39:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    allowedHosts: 'all',
+    hmr: {
+      clientPort: 443
+    }
   },
   build: {
     outDir: 'dist',
